d2e33bea76fe5adfcc1387ade5274627738c1b17
Gabriel <gabrielsideprojects@gmail.com>
2023-02-11 09:41:51 -0300

    Add status for our requests

--- src/server.js
+++ src/server.js
@@ -15,20 +15,39 @@ import http from 'node:http'
 // GET /users => Buscando usuários do back-end
 // post /USERS => Criar um usuário no back-end
 
+// Stateful: Vai ter alguma informação guardada em memória.
+// Stateless
+
+// JSON - Javascript Object Notation
+
+// Cabeçalhos (Requisição/resposta) => Metadados.
+
+// HTTP Status Code - Importância semântica entre front e back-end.
+//201 - Created
+
+const users = []
+
 const server = http.createServer((req,res)=> {
     const {method, url} = req
 
     if(method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuários')
+        return res.
+        setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
     }
 
     if(method === 'POST' && url === '/users') {
-        return res.end('Criação de usuário')
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
+
+        return res.writeHead(201).end()
     }
 
-    console.log(method, url)
 
-    return res.end('Hello World')
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
