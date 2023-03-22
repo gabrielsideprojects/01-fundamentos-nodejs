0adeddde7810b60ba6b344cff79c6398ecc3c835
Gabriel <gabrielsideprojects@gmail.com>
2023-03-22 18:52:58 -0300

    Add buffer explanation

---
+++ streams/buffer.js
@@ -0,0 +1,8 @@
+/**
+ * Buffer:Representação de um espaço na memória do computador para transitar dados de 
+ * uma forma muito rápida. Escrever e ler da memória de forma binário ( Altamente performático).
+ */
+
+const buf = Buffer.from("ok")
+
+console.log(buf.toJSON())
