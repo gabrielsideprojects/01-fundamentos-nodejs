9356558a4f1fd18bd323387a4fd5e573d38de69b
Gabriel <gabrielsideprojects@gmail.com>
2023-04-08 17:50:39 -0300

    Add methdo to extract query params

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import http from 'node:http'
 
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 //Query Parameters: URL Stateful => Filtros, paginação ,não-obrigatório;
 // Route Parameters: https://localhost:3333/users/1 -> Geralmente identificação de recurso;
@@ -49,7 +50,10 @@ const server = http.createServer(async (req,res)=> {
     if (route) {
         const routeParams = req.url.match(route.path)
 
-        req.params = { ...routeParams.groups}
+        const {query, ...params} = routeParams.groups
+
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
 
         return route.handler(req, res)
     }
